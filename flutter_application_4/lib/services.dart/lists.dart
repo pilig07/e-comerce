733c121fc15041751784aa14389567dde1053b8a
GabrielMontalvo <contact.hhmon@gmail.com>
2022-11-28 13:09:03 -0600

    Mod lists

--- flutter_application_4/lib/services.dart/lists.dart
+++ flutter_application_4/lib/services.dart/lists.dart
@@ -5,24 +5,24 @@ List habitList = [
     false, //If it is complete or not
     "Tomar agua", //The title of the habit
     "Beber al menos 3lts de agua", //The subtitle of the habit,
-    Icon(Icons.local_drink) //The icon of the habit
+    const Icon(Icons.local_drink) //The icon of the habits
   ],
   [
     false, //If it is complete or not
     "Leer", //The title of the habit
     "Leer al menos 20 paginas de un libro", //The subtitle of the habit,
-    Icon(Icons.book) //The icon of the habit
+    const Icon(Icons.book) //The icon of the habit
   ],
   [
-    false, //If it is complete or not
+    true, //If it is complete or not
     "Entrenar", //The title of the habit
     "Ir al gimnasio o hacer cardio", //The subtitle of the habit,
-    Icon(Icons.sports_gymnastics), //The icon of the habit
+    const Icon(Icons.sports_gymnastics), //The icon of the habit
   ],
   [
     false, //If it is complete or not
     "Aprender frances", //The title of the habit
     "Estudiar frances por 20 min", //The subtitle of the habit,
-    Icon(Icons.language), //The icon of the habit
+    const Icon(Icons.language), //The icon of the habit
   ],
 ];
